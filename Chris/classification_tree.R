473df4ab702aa452e477325638b42bbb4d773315
Christopher Carlson <christopher.s.carlson@wmich.edu>
2015-06-26 14:03:40 -0400

    Got basics of random forest working, also added to classification_tree.R considerably, but that file is a bit of a mess right now.  I am in the process of refactoring the code to use the "apply" functions instead of inscrutable looping structures.  Currently I am working out how to aggregate the responces of each n-fold classification into an easy to digest format.

--- Chris/classification_tree.R
+++ Chris/classification_tree.R
@@ -1,22 +1,64 @@
 library(rpart)
+library(plyr)	
+
+# Function to generate a fit given a data frame object from the 
+# mushrooms data set.
+
+grow_tree <- function(data_frame) {
+
+	rpart(edibility~., data=data_frame, method="class")
+
+}
+
+get_fold <- function(list_of_folds, skip) {
+
+	list_of_folds[[integer_value]]
+}
+
+print_summary <- function(item) {
+	print(summary(item))
+}
+
+test_fit <- function(fit, test_data) {
+	predict(fit, test_data, type='class')
+
+}
 
 for( i in 1:length(folds))
 {
-   fit <- rpart(edibility~., data=folds[[i]], method="class")
+	# To Improve Code Clarity, name i 'test' and the rest 'train'
+	test  <- folds[[i]]
+	train <- folds[c(seq(1:length(folds)))[-i]]
+	
+	fit <- grow_tree(test)
+	
+	fits  <- llply(train, FUN=grow_tree)
+	preds <- llply(fits, FUN=test_fit, test_data=test)
+	
+	lapply(preds, FUN=print_summary)
+	indi <- test_fit(fit, test)
+	print("")
+	print("Individual summary")
+	summary(indi)
+	print("Names Indi")
+	names(indi)
+	
+	
+	par(mfrow=c(1,2), xpd=NA)
+	plot(fit, uniform=TRUE, main="Classification Tree for Mushroom Edibility")
+	text(fit, use.n=TRUE, all=TRUE, cex=.8)
+	print(fit$variable.importance)
+	
+	print('\nPress Enter to Prune the Tree')
+	readline()
+	post(fit, file=paste0("results_temp/fit_", i), title="Classification Tree for Mushroom Edibility")
 
-   printcp(fit)
-	 plotcp(fit)
+	pfit <- prune(fit, cp=fit$cptable[which.min(fit$cptable[,"xerror"]), "CP"])
 
+	plot(pfit, uniform=TRUE, main="Pruned Classification Tree for Mushroom Edibility")
+	text(pfit, use.n=TRUE, all=TRUE, cex=.8)
+	post(pfit, file=paste0("results_temp/pfit_", i))
 
-	 plot(fit, uniform=TRUE, main="Classification Tree for Mushroom Edibility")
-   text(fit, use.n=TRUE, all=TRUE, cex=.8)
-	 readline()
-	 post(fit, file=paste0("results_temp/fit_", i), title="Classification Tree for Mushroom Edibility")
-
-	 pfit <- prune(fit, cp=fit$cptable[which.min(fit$cptable[,"xerror"]), "CP"])
-
-   plot(pfit, uniform=TRUE, main="Pruned Classification Tree for Mushroom Edibility")
-   text(pfit, use.n=TRUE, all=TRUE, cex=.8)
-	 post(pfit, file=paste0("results_temp/pfit_", i))
-	 readline()
+	print("\nPress Enter to run next fold")
+	readline()
 }
